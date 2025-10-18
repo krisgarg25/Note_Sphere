abc6f5e9de502a14797357cccfa304df6b1ef116
Kris Garg <krisgarg25@gmail.com>
2025-10-18 20:00:54 +0530

    check 2

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit appndex.tsx to edit this screen.</Text>
+      <Text>Edit appndex.tsx to edi this screen.</Text>
     </View>
   );
 }
